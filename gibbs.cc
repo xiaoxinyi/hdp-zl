8c3c702faf0eb655c45fc0cdbc6b7bf4eff3343a
Zhang Li <xiaoxin88214@163.com>
2016-03-08 00:26:56 +0800

    modify gibbs.cc

--- gibbs.cc
+++ gibbs.cc
@@ -1,16 +1,3 @@
-// Copyright 2012 Google Inc. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 
 #include <assert.h>
@@ -21,7 +8,7 @@
 #include "gibbs.h"
 
 #define REP_NO 100
-#define DEFAULT_HYPER_LAG 1
+#define DEFAULT_HYPER_LAG 0
 #define DEFAULT_SHUFFLE_LAG 100
 #define DEFAULT_SAMPLE_GAMMA 0
 #define DEFAULT_SAMPLE_ETA 0
@@ -117,6 +104,8 @@ void GibbsSampler::ReadGibbsInput(
   gibbs_state->setSampleAlpha(sample_alpha);
   gibbs_state->setSampleGamma(sample_gamma);
   gibbs_state->setCorpus(corpus);
+
+  AllTopics.GetInstance().setEta(eta);
 
 }
 
